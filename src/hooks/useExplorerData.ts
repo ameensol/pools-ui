305fdac09f03c13dc48cb5cd6d894d77d2764696
J. Farid <jrab227@gmail.com>
2023-03-07 15:28:36 -0500

    Wrap useExplorerData in useEffect.

--- src/hooks/useExplorerData.ts
+++ src/hooks/useExplorerData.ts
@@ -8,6 +8,21 @@ import {
   useSubsetDataByNullifier
 } from '../hooks';
 
+export interface ExplorerDataI {
+  accessList: AccessList;
+  includedDeposits: Commitment[];
+  excludedDeposits: Commitment[];
+}
+
+const defaultExplorerData: ExplorerDataI = {
+  accessList: AccessList.fullEmpty({
+    accessType: 'blocklist',
+    subsetLength: 0
+  }),
+  includedDeposits: [],
+  excludedDeposits: []
+};
+
 export function useExplorerData() {
   const { commitments } = useCommitments();
   const [recentWithdrawal] = useAtom(recentWithdrawalAtom);
@@ -18,52 +33,41 @@ export function useExplorerData() {
 
   const { nullifier, subsetRoot } = debouncedRecentWithdrawal;
   const { accessType, bitLength, subsetData: data } = subsetMetadata;
-
-  if (
-    !nullifier ||
-    !subsetRoot ||
-    !debouncedCommitments.length ||
-    !accessType ||
-    isNaN(bitLength) ||
-    data.length === 0
-  ) {
-    return {
-      accessList: AccessList.fullEmpty({
-        accessType: 'blocklist',
-        subsetLength: 0
-      }),
-      includedDeposits: [],
-      excludedDeposits: []
-    };
-  }
-
-  const accessList = new AccessList({
-    accessType,
-    bytesData: { bitLength, data }
-  });
-  let includedDeposits: Commitment[];
-  let excludedDeposits: Commitment[];
-  if (commitments.length >= accessList.length) {
-    let start = 0;
-    const end = accessList.length;
-    if (end > 30) {
-      start = end - 30;
+  
+  const [explorerData, setExplorerData] = useState<ExplorerDataI>(defaultExplorerData);
+  
+  useEffect(() => {
+    const accessList = new AccessList({
+      accessType,
+      bytesData: { bitLength, data }
+    });
+    let includedDeposits: Commitment[];
+    let excludedDeposits: Commitment[];
+    if (commitments.length >= accessList.length) {
+      let start = 0;
+      const end = accessList.length;
+      if (end > 30) {
+        start = end - 30;
+      }
+      const c = commitments.slice(start, end);
+      includedDeposits = c.filter(
+        ({ leafIndex }) => accessList.subsetData[Number(leafIndex)] === 0
+      );
+      excludedDeposits = c.filter(
+        ({ leafIndex }) => accessList.subsetData[Number(leafIndex)] === 1
+      );
+    } else {
+      includedDeposits = [];
+      excludedDeposits = [];
     }
-    const c = commitments.slice(start, end);
-    includedDeposits = c.filter(
-      ({ leafIndex }) => accessList.subsetData[Number(leafIndex)] === 0
-    );
-    excludedDeposits = c.filter(
-      ({ leafIndex }) => accessList.subsetData[Number(leafIndex)] === 1
-    );
-  } else {
-    includedDeposits = [];
-    excludedDeposits = [];
-  }
+    setExplorerData({
+      accessList,
+      includedDeposits,
+      excludedDeposits
+    });
 
-  return {
-    accessList,
-    includedDeposits,
-    excludedDeposits
-  };
+  }, [nullifier, subsetRoot, debouncedCommitments, accessType, bitLength, data]);
+
+  return explorerData;
+
 }
